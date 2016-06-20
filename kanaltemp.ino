233e83057afb3c39745d107bb17269804a432575
Ingomar Otter <iotter@mac.com>
2016-06-20 14:42:05 +0200

    Some powersaving means.

--- kanaltemp.ino
+++ kanaltemp.ino
@@ -1,10 +1,10 @@
-
 #include <Arduino.h>
 #include <RTCZero.h>
 #include <Sodaq_RN2483.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <math.h>
+#include <SPI.h>
 
 //#include "cmp.h"
 
@@ -18,18 +18,13 @@ RTCZero rtc;
 #define INT_LINE 4
 #define POL_LINE 5
 
-static int theUpdateRate =  2 * 60; //sec
+static int theUpdateRate =  30 * 60; //sec
 
 #define debugSerial SerialUSB
 #define loraSerial Serial1
-#define ONE_WIRE_BUS 1
+#define ONE_WIRE_BUS 3
 
 const static uint16_t recv_buffer_sz = 32;
-
-#ifndef DUMMY_TEMP
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-#endif
 
 #ifdef DEBUG
 #define DEB(x) debugSerial.println(x);
@@ -37,22 +32,35 @@ DallasTemperature sensors(&oneWire);
 #define DEB(x)
 #endif
 
+#ifndef DEBUG
 #define SEND_LED(x) digitalWrite(LED_BUILTIN,x)
+#else
+#endig  SEND_LED(x)
 
-#define CMD_SET_UPDATE_RATE  0x01 //payload: new rate (uint8) in minutes
+enum Commands {
+  CMD_NOP =0x00,             // payload:none; no-operation
+  CMD_SET_UPDATE_RATE =0x01, //payload: uint8 - new update rate in minutes
+  CMD_GET_STATS = 0x02,      //payload: none; replies with some statistics (not implemented)
+  CMD_RESET=0xff             //payload: none; board performs reset (not implemented)
+};
 
+//#define CMD_SET_UPDATE_RATE  0x01 
+//#define CMD_GET_STATS        0x02 
+//#define CMD_RESET            0xFF
 
 static int32_t ccsum = 0;
 static bool intCc = false;
 static bool intAlarm = false;
 
+static struct  {
+   uint16_t messagesTransmitted = 0;
+} kanalStats;
 
 static unsigned long last_cc_tick = 0;
 static float last_pwr_consumption = 0;
 static uint16_t numTicks = 0;
 static uint32_t totalTickTime = 0;
 static bool direction; //true=charge
-
 
 static uint8_t recieveBuffer[recv_buffer_sz];
 
@@ -70,8 +78,10 @@ void setup()
   digitalWrite(BEE_VCC, HIGH);
 
   //turn on on board LED to signal startup
+#ifdef DEBUG
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
+#endif
 
   //coulomb counter
   pinMode(INT_LINE, INPUT);
@@ -96,11 +106,11 @@ void setup()
     DEB("Connection to the network failed!");
   }
 
-#ifndef DUMMY_TEMP
-  sensors.begin();
-#endif
+
   rtc.begin();
   last_cc_tick = millis();
+
+  DFlashUltraDeepSleep();  //send SPI flash to ultra deep sleep
 }
 
 void alarmMatch()
@@ -124,9 +134,7 @@ void lowPowerSleep(uint16_t sec) {
   rtc.attachInterrupt(&alarmMatch);
   rtc.enableAlarm(rtc.MATCH_HHMMSS);
 
-  // Disable USB
   DEB("standby");
-
   do {
     intCc = false;
     rtc.standbyMode();  //sleep....
@@ -163,12 +171,17 @@ void processCommand(uint16_t numBytesRecieved)
       if (numBytesRecieved == 2 ) {
         uint8_t newRate = recieveBuffer[1];
         theUpdateRate = newRate * 60;
+        if(theUpdateRate <60) {  //dont permit an update rate < 1 minute
+          theUpdateRate = 60; 
+        }
         DEB("CMD: new update rate");
         DEB(theUpdateRate);
       } else {
         DEB("incorret payload for command");
       }
       break;
+    case CMD_NOP: //nop
+    break;
     default:
       DEB("unkown command");
       break;
@@ -180,8 +193,19 @@ void loop()
   int16_t transmitTemp;
 
 #ifndef DUMMY_TEMP
+  //power up switch GROVE rail
+  pinMode(VCC_SW, OUTPUT);
+  digitalWrite(VCC_SW, HIGH);
+  delay(100);
+  OneWire oneWire(ONE_WIRE_BUS);
+  DallasTemperature sensors(&oneWire);
+  sensors.begin();
   sensors.requestTemperatures();
   float temp = sensors.getTempCByIndex(0);
+
+  oneWire.depower();  //switch off parasitic power (if used)
+  digitalWrite(VCC_SW, LOW); //power down GROVE rail
+
   transmitTemp = (int16_t) 10 * temp;
 #else
   transmitTemp = DUMMY_TEMP * 10;
@@ -233,6 +257,7 @@ void loop()
   {
     case NoError:
       DEB("/");
+      kanalStats.messagesTransmitted++;
       break;
     case NoResponse:
       DEB("There was no response from the device.");
@@ -246,19 +271,22 @@ void loop()
       break;
     case InternalError:
       DEB("Oh No! This shouldn't happen. Something is really wrong! Try restarting the device!\r\nThe program will now halt.");
-      while (1) {}; //TODO handle this better
+      delay(30000);
+      autonomoReset();
       break;
     case Busy:
       DEB("The device is busy. Sleeping for 10 extra seconds.");
-      delay(10000);
+      delay(60000);
       break;
     case NetworkFatalError:
       DEB("There is a non-recoverable error with the network connection. You should re-connect.\r\nThe program will now halt.");
-      while (1) {};  //TODO handle this better
+      delay(60000);
+      autonomoReset();
       break;
     case NotConnected:
       DEB("The device is not connected to the network. Please connect to the network before attempting to send data.\r\nThe program will now halt.");
-      while (1) {};  //TODO handle this better
+      delay(60000);
+      autonomoReset();
       break;
     case NoAcknowledgment:
       DEB("There was no acknowledgment sent back!");
@@ -274,5 +302,62 @@ void loop()
   }
 
   lowPowerSleep(theUpdateRate);
+}
+
+
+void DFlashUltraDeepSleep()
+{
+  // SPI initialisation
+  SPI.begin();
+
+  // Initialise the CS pin for the data flash
+  pinMode(SS_DFLASH, OUTPUT);
+  digitalWrite(SS_DFLASH, HIGH);
+
+  transmit(0x00); // In case already in sleep, wake
+  transmit(0x79); // Now enter sleep
+  //transmit(0x00); // Wake again, for testing
+
+  // SPI end
+  SPI.end();
+
+  // Resets the pins used
+  resetSPIPins();
+}
+
+void transmit(uint8_t val)
+{
+  SPISettings settings;
+  SPI.beginTransaction(settings);
+  digitalWrite(SS_DFLASH, LOW);
+
+  SPI.transfer(val);
+
+  digitalWrite(SS_DFLASH, HIGH);
+  SPI.endTransaction();
+
+  delayMicroseconds(1000);
+}
+
+void resetSPIPins()
+{
+  resetPin(MISO);
+  resetPin(MOSI);
+  resetPin(SCK);
+  resetPin(SS);
+}
+
+void resetPin(uint8_t pin)
+{
+  PORT->Group[g_APinDescription[pin].ulPort].PINCFG[g_APinDescription[pin].ulPin].reg = (uint8_t)(0);
+  PORT->Group[g_APinDescription[pin].ulPort].DIRCLR.reg = (uint32_t)(1 << g_APinDescription[pin].ulPin);
+  PORT->Group[g_APinDescription[pin].ulPort].OUTCLR.reg = (uint32_t) (1 << g_APinDescription[pin].ulPin);
+}
+
+void autonomoReset() {
+  __DSB();
+  SCB->AIRCR  = 0x05FA0004;
+  __DSB();
+  while (1);
 }
 
